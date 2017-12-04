e61236ac64dfa543f332f349658c2722674f71f5
Tianji Li <skyahead@gmail.com>
2017-12-04 15:47:07 -0500

    keep file names in a file

--- src/main/java/com/github/jcustenborder/kafka/connect/spooldir/SpoolDirSourceTask.java
+++ src/main/java/com/github/jcustenborder/kafka/connect/spooldir/SpoolDirSourceTask.java
@@ -44,7 +44,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
